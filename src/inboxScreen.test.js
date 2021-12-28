ce8d2a9fc65be68510296a53f895cbbe21cc160a
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2021-12-28 15:21:46 +0000

    jest

---
+++ src/inboxScreen.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, waitFor, cleanup, within, fireEvent } from '@testing-library/react';
+import { composeStories } from '@storybook/testing-react';
+import { getWorker } from 'msw-storybook-addon';
+import * as stories from './InboxScreen.stories';
+
+describe('InboxScreen', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  // Clean up after all tests are done, preventing this
+  // interception layer from affecting irrelevant tests
+  afterAll(() => getWorker().close());
+
+  const { Default } = composeStories(stories);
+
+  it('should pin a task', async () => {
+    const { queryByText, getByRole } = render(<Default />);
+
+    await waitFor(() => {
+      expect(queryByText('You have no tasks')).not.toBeInTheDocument();
+    });
+
+    const getTask = () => getByRole('listitem', { name: 'Export logo' });
+
+    const pinButton = within(getTask()).getByRole('button', { name: 'pin' });
+
+    fireEvent.click(pinButton);
+
+    const unpinButton = within(getTask()).getByRole('button', {
+      name: 'unpin',
+    });
+
+    expect(unpinButton).toBeInTheDocument();
+  });
+});
